79a7e9aa7584ddd30d86bf02bb9a1e602d547a62
Khalitov62 <tu7m5rk73code.edu.az>
2022-03-10 13:15:13 +0400

    C# texts added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max()); 
+        }
+        public static int Max()
+        {
+            int[] arr = new int[] { 5, 10, 9, 50, 2000, 1000 };
+            int max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            return max;
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int numNatural = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Natural(num,numNatural));
+        }
+       public static int Natural(int num, int numNatural)
+        {
+            int numCopy = num;
+            int count = 0;
+            int reqemsayi = 1;
+            while (numCopy >= 1)
+                count++;
+            {
+               for (int i = 0; i < count; i++)
+                {
+                    reqemsayi *= 10;
+                    {
+                        num = (numNatural * reqemsayi) + num;
+                    }
+                }
+                return num;
+            }
+        }
+    }
+}
